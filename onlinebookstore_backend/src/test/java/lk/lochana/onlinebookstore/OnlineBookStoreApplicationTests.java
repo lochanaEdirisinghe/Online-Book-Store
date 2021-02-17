65831403add705cd41dff8938c3bd73b372da9ac
Lochana Edirisinghe <edirisinghelochana5@gmail.com>
2021-02-17 08:38:12 +0530

    initial commit

---
+++ onlinebookstore_backend/src/test/java/lk/lochana/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.lochana.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
